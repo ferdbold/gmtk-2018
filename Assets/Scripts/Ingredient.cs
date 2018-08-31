724722c0b19174385c968f975b245f90eb0cc2aa
Frédéric Bolduc <bolducfrederic@gmail.com>
2018-08-31 19:14:26 -0400

    Ingredient definition

---
+++ Assets/Scripts/Ingredient.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class Ingredient : MonoBehaviour {
+
+    public Color _color;
+
+    [Range(0, 1)]
+    public float _solidity;
+
+    [Range(0, 1)]
+    public float _length;
+
+    [Range(0, 1)]
+    public float _rugosity;
+}
