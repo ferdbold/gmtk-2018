c5a489385f3ed705dcd0c1812c735b4a45a7c1da
Alexis Lessard <alex3521@hotmail.fr>
2018-08-31 20:51:32 -0400

    Basic Movement

---
+++ Assets/Scripts/Core/Manager/InventoryManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryManager : BaseManager<InventoryManager> {
+
+    private Transform _grabbedTransform;
+
+
+    #region LIFECYCLE
+    public override void OnStartManager() {
+
+    }
+
+    public override void OnRegisterCallbacks() {
+
+    }
+
+    public override void OnUnregisterCallbacks() {
+
+    }
+
+
+    public override void OnUpdateManager(float deltaTime) {
+
+    }
+    #endregion
+
+    #region Gameplay
+
+    #endregion
+}
--- Assets/Scripts/Core/Manager/PlayerManager.cs
+++ Assets/Scripts/Core/Manager/PlayerManager.cs
@@ -5,13 +5,20 @@ using UnityEngine;
 public class PlayerManager : BaseManager<PlayerManager> {
 
     [SerializeField] private Transform _player;
+
+    [Header("Movement")]
     [SerializeField] private AnimationCurve _movementCurve;
+    [SerializeField] private float _speed = 1f;
+    [SerializeField] private float _gravity = 1f;
+    [SerializeField] private float maxSpeed = 3f;
+    private float _currentVelocity = 0;
 
     #region LIFECYCLE
     public override void OnStartManager() 
     {
         _player.position = Vector3.zero;
         _player.rotation = Quaternion.identity;
+        _currentVelocity = 0f;
     }
 
     public override void OnRegisterCallbacks() 
@@ -25,19 +32,36 @@ public class PlayerManager : BaseManager<PlayerManager> {
     }
 
 
-    public override void OnUpdateManager(float deltaTime)
-    {
-        float xAxis = Input.GetAxis("Horizontal");
-        OnPlayerMove(Mathf.Sign(xAxis) * _movementCurve.Evaluate(Mathf.Abs(xAxis)));
+    public override void OnUpdateManager(float deltaTime) {
+        HandleHorizontalMovement();
     }
     #endregion
 
 
-    #region 
+    #region Gameplay
 
-    private void OnPlayerMove(float direction)
-    {
-        _player.Rotate(Vector3.up, direction);
+    private void HandleHorizontalMovement() {
+        float xAxis = 0f;
+        if (Input.GetAxis("Horizontal") == 1) xAxis = 1;
+        else if (Input.GetAxis("Horizontal") == -1) xAxis = -1;
+
+
+        if (Mathf.Approximately(xAxis, 0f)) {
+            if (Mathf.Abs(_currentVelocity) > 0) {
+                _currentVelocity -= _gravity * Mathf.Sign(_currentVelocity);
+                if (Mathf.Abs(_currentVelocity) < _gravity) _currentVelocity = 0f;
+            }
+        } else {
+            _currentVelocity += Mathf.Sign(xAxis) * _speed * _movementCurve.Evaluate(Mathf.Abs(xAxis));
+        }
+        _currentVelocity = Mathf.Clamp(_currentVelocity, -maxSpeed, maxSpeed);
+
+        OnPlayerMove();
+    }
+
+    private void OnPlayerMove()
+    {       
+        _player.Rotate(Vector3.up, _currentVelocity);
     }
 
     #endregion
