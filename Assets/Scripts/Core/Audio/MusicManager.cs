2e095f9f8336bff2723742d25bb44686a7625f9e
Alexis Lessard <alex3521@hotmail.fr>
2018-09-02 12:37:37 -0400

    added songs

--- Assets/Scripts/Core/Audio/MusicManager.cs
+++ Assets/Scripts/Core/Audio/MusicManager.cs
@@ -11,52 +11,60 @@ public class MusicManager : MonoBehaviour
     public AudioClip EndSong;
 
 
-    public AudioSource source;
-    private float _targetVolume = 1f;
+    public AudioSource sourceMenu;
+    public AudioSource sourceGame;
+    public AudioSource sourceEnd;
+
+    public float _targetVolumeMenu = 1f;
+    public float _targetVolumeGame = 1f;
+    public float _targetVolumeEnd = 1f;
 
     private void OnEnable()
     {
         ObjectiveManager.OnGameStarted += OnGameStarted;
-        ObjectiveManager.OnGameStarted += OnGameEnded;
+        ObjectiveManager.OnGameEnded += OnGameEnded;
     }
     private void OnDisable()
     {
         ObjectiveManager.OnGameStarted -= OnGameStarted;
-        ObjectiveManager.OnGameStarted -= OnGameEnded;
-
+        ObjectiveManager.OnGameEnded -= OnGameEnded;
     }
 
     private void OnGameStarted() {
         PlayGame();
     }
-    private void OnGameEnded() { 
-
+    private void OnGameEnded() {
+        EndGame();
     }
 
 
     void Start()
-    {       
-        source.loop = true;
-        source.volume = _targetVolume;
-    }
-
-    public AudioSource[] GetAudioSources()
     {
-        AudioSource[] sources = new AudioSource[1];
-        sources[0] = source;
-        return sources;
+        sourceMenu.volume = 0;
+        sourceGame.volume = 0;
+        sourceEnd.volume = 0;
+
+        PlayMenu();
     }
 
     private void PlayMenu() {
-        source.clip = MenuSong;
-        source.Play();
+        sourceMenu.clip = MenuSong;
+        sourceMenu.Play();
+        sourceMenu.DOFade(1f, 0.5f);
+
     }
     private void PlayGame() {
-        source.clip = GameSong;
-        source.Play();
+        sourceGame.clip = GameSong;
+        sourceGame.DOFade(_targetVolumeGame, 1.5f);
+        sourceGame.Play();
+
+        sourceMenu.DOFade(0f, 1f);
     }
     private void EndGame() {
-        source.clip = EndSong;
-        source.Play();
+        sourceEnd.clip = EndSong;
+        sourceEnd.Play();
+        sourceEnd.DOFade(_targetVolumeEnd, 1.5f);
+
+        sourceGame.DOFade(0f, 1f);
     }
 }
