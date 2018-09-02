5f9e64041f3440f5251fbcc4004eac3b5cb82975
Frédéric Bolduc <me@ferdbold.com>
2018-09-01 20:13:41 -0400

    Grade system, part 1

--- Assets/Scripts/UI/RecipeWidget.cs
+++ Assets/Scripts/UI/RecipeWidget.cs
@@ -38,6 +38,24 @@ public class RecipeWidget : MonoBehaviour {
 
     public void OnRecipeShipped(ObjectiveManager.SRecipeScore recipeScore)
     {
+        for (int i = 0; i < _IngredientWidgets.Count; i++)
+        {
+            _IngredientWidgets[i].SetGrade(GetGrade(recipeScore._IngredientScores[i]._globalScore));
+        }
+
         Debug.Log("Recipe SHIPPED");
+    }
+
+    public Sprite GetGrade(float score)
+    {
+        foreach (SGrade grade in _grades)
+        {
+            if (score >= grade._scoreThreshold)
+            {
+                return grade._sprite;
+            }
+        }
+
+        return null;
     }
 }
